6a2a2fbfafa341258d1aff1fa6618143290bb601
Bishal Santra <bsantraigi@gmail.com>
2022-02-23 14:48:06 +0530

    config generation script update + all year configs

--- generate_configs.py
+++ generate_configs.py
@@ -1,11 +1,16 @@
 prefix = \
-"""run_dir: "scratch/prod/{yr}/"
+"""run_dir: "scratch/prod_{yr}/"
+max_concurrent_downloads: 4
 min_year: "{yr}"
 max_year: "{yr}"
 min_year_min_month: 1
 max_year_max_month: 12
-data_zip_path: dstc8-reddit-corpus-{yr}.zip"""
+data_zip_path: dstc8-reddit-corpus-{yr}.zip
+"""
 
-for yr in range(2010, 2021):
+with open("configs/prod/template_2016.yaml") as f:
+    postfix = f.read()
+
+for yr in range(2005, 2021):
     with open(f"configs/prod/{yr}.yaml", "w") as f:
-        f.write(prefix.format(yr=yr))
+        f.write(prefix.format(yr=yr) + postfix)
